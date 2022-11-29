b3ca52efe9a2a5e5378fc6c711deba79aa9b8c24
demicoderrr <119343705+demicoderrr@users.noreply.github.com>
2022-11-29 23:04:33 +0800

    Add files via upload

---
+++ src/services/DokterService.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3001/dokter";
+
+const getAll = async () => {
+    const response = await axios.get(baseUrl);
+    return response.data;
+}
+
+const addData = async (data) => {
+    const response = await axios.post(baseUrl, data);
+    return response.data;
+}
+
+const updateData = async (id, data) => {
+    const response = await axios.put(`${baseUrl}/${id}`, data);
+    return response.data;
+}
+
+const removeData =  async (id) => {
+    const response = await axios.delete(`${baseUrl}/${id}`);
+    return response.data;
+}
+
+export default {getAll, addData, updateData, removeData};
